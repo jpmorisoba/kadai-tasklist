72edd89bd3b4094f9c963adaac0c5ef6ae04e2e6
morisoba <takao@washbuil.com>
2017-12-09 04:31:35 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks if logged_in?
   end
 
   def show
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -53,6 +54,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content,:status)
+  end
+  
+  def correct_user
+    if @task.user != current_user
+      redirect_to root_url
+    end
   end
 end
 
