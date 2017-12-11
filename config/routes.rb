0a1c2ef7bb925a25d5970bda5c67d228983a16fd
morisoba <takao@washbuil.com>
2017-12-11 10:48:16 +0000

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
   root to: 'tasks#index'
 
   get 'login', to: 'sessions#new'
@@ -10,3 +10,4 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :tasks
 end
+
